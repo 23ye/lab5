29175fb3746b73b5416e51cf9d22aab745954ec6
unknown <2431002620@qq.com>
2026-01-01 16:11:44 +0800

    part2

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ChatClient/ui_mainwindow.h
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ChatClient/ui_mainwindow.h
@@ -11,9 +11,18 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,24 +31,182 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QMenuBar *menubar;
-    QStatusBar *statusbar;
+    QVBoxLayout *verticalLayout;
+    QLabel *titleLabel;
+    QStackedWidget *stackedWidget;
+    QWidget *LoginPage;
+    QGridLayout *gridLayout_2;
+    QSpacerItem *verticalSpacer_2;
+    QSpacerItem *horizontalSpacer;
+    QFrame *loginFrame;
+    QVBoxLayout *verticalLayout_2;
+    QGridLayout *gridLayout;
+    QLabel *ServerEdit;
+    QLineEdit *lineEdit;
+    QLabel *UserNameEdit;
+    QLineEdit *lineEdit_2;
+    QSpacerItem *verticalSpacer;
+    QPushButton *LoginButton;
+    QSpacerItem *horizontalSpacer_2;
+    QSpacerItem *verticalSpacer_3;
+    QWidget *ChatPage;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QTextEdit *textEdit;
+    QListWidget *listWidget;
+    QHBoxLayout *horizontalLayout_2;
+    QLineEdit *lineEdit_3;
+    QPushButton *SendButton;
+    QPushButton *ExitButton;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
         MainWindow->resize(800, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("#titleLabel{\n"
+"background:white;\n"
+"color:bule;\n"
+"font-size:20px;\n"
+"border:none;\n"
+"border-bottom:1px solid black;\n"
+"padding:5px;\n"
+"font:24pt\"\351\273\221\344\275\223\";\n"
+"color:rgb(255,255,255);\n"
+"background-color:rgba(0,170,127,255)\n"
+"}\n"
+"\n"
+"#mainFrame{\n"
+"border:none;\n"
+"background:white;\n"
+"}\n"
+"\n"
+"#loginFrame{\n"
+"background:green;\n"
+"border:1px soild gray;\n"
+"padding:10px;\n"
+"border-radius:25px;\n"
+"}"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName("verticalLayout");
+        titleLabel = new QLabel(centralwidget);
+        titleLabel->setObjectName("titleLabel");
+
+        verticalLayout->addWidget(titleLabel);
+
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName("stackedWidget");
+        LoginPage = new QWidget();
+        LoginPage->setObjectName("LoginPage");
+        gridLayout_2 = new QGridLayout(LoginPage);
+        gridLayout_2->setObjectName("gridLayout_2");
+        verticalSpacer_2 = new QSpacerItem(20, 204, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
+
+        gridLayout_2->addItem(verticalSpacer_2, 0, 1, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(248, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer, 1, 0, 1, 1);
+
+        loginFrame = new QFrame(LoginPage);
+        loginFrame->setObjectName("loginFrame");
+        loginFrame->setFrameShape(QFrame::Shape::StyledPanel);
+        loginFrame->setFrameShadow(QFrame::Shadow::Raised);
+        verticalLayout_2 = new QVBoxLayout(loginFrame);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName("gridLayout");
+        ServerEdit = new QLabel(loginFrame);
+        ServerEdit->setObjectName("ServerEdit");
+
+        gridLayout->addWidget(ServerEdit, 0, 0, 1, 1);
+
+        lineEdit = new QLineEdit(loginFrame);
+        lineEdit->setObjectName("lineEdit");
+
+        gridLayout->addWidget(lineEdit, 0, 1, 1, 1);
+
+        UserNameEdit = new QLabel(loginFrame);
+        UserNameEdit->setObjectName("UserNameEdit");
+
+        gridLayout->addWidget(UserNameEdit, 1, 0, 1, 1);
+
+        lineEdit_2 = new QLineEdit(loginFrame);
+        lineEdit_2->setObjectName("lineEdit_2");
+
+        gridLayout->addWidget(lineEdit_2, 1, 1, 1, 1);
+
+
+        verticalLayout_2->addLayout(gridLayout);
+
+        verticalSpacer = new QSpacerItem(20, 10, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Fixed);
+
+        verticalLayout_2->addItem(verticalSpacer);
+
+        LoginButton = new QPushButton(loginFrame);
+        LoginButton->setObjectName("LoginButton");
+
+        verticalLayout_2->addWidget(LoginButton);
+
+
+        gridLayout_2->addWidget(loginFrame, 1, 1, 1, 1);
+
+        horizontalSpacer_2 = new QSpacerItem(248, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer_2, 1, 2, 1, 1);
+
+        verticalSpacer_3 = new QSpacerItem(20, 203, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
+
+        gridLayout_2->addItem(verticalSpacer_3, 2, 1, 1, 1);
+
+        stackedWidget->addWidget(LoginPage);
+        ChatPage = new QWidget();
+        ChatPage->setObjectName("ChatPage");
+        verticalLayout_3 = new QVBoxLayout(ChatPage);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        textEdit = new QTextEdit(ChatPage);
+        textEdit->setObjectName("textEdit");
+
+        horizontalLayout->addWidget(textEdit);
+
+        listWidget = new QListWidget(ChatPage);
+        listWidget->setObjectName("listWidget");
+        listWidget->setMaximumSize(QSize(120, 16777215));
+
+        horizontalLayout->addWidget(listWidget);
+
+
+        verticalLayout_3->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        lineEdit_3 = new QLineEdit(ChatPage);
+        lineEdit_3->setObjectName("lineEdit_3");
+
+        horizontalLayout_2->addWidget(lineEdit_3);
+
+        SendButton = new QPushButton(ChatPage);
+        SendButton->setObjectName("SendButton");
+
+        horizontalLayout_2->addWidget(SendButton);
+
+        ExitButton = new QPushButton(ChatPage);
+        ExitButton->setObjectName("ExitButton");
+
+        horizontalLayout_2->addWidget(ExitButton);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_2);
+
+        stackedWidget->addWidget(ChatPage);
+
+        verticalLayout->addWidget(stackedWidget);
+
         MainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 21));
-        MainWindow->setMenuBar(menubar);
-        statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName("statusbar");
-        MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
 
@@ -49,6 +216,12 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        titleLabel->setText(QCoreApplication::translate("MainWindow", "\346\233\276\347\202\253\346\235\260\347\232\204\350\201\212\345\244\251\345\256\244", nullptr));
+        ServerEdit->setText(QCoreApplication::translate("MainWindow", "\346\234\215\345\212\241\345\231\250\345\234\260\345\235\200\357\274\232", nullptr));
+        UserNameEdit->setText(QCoreApplication::translate("MainWindow", "\346\230\265\347\247\260\357\274\232", nullptr));
+        LoginButton->setText(QCoreApplication::translate("MainWindow", "\347\231\273\345\275\225\350\201\212\345\244\251\345\256\244", nullptr));
+        SendButton->setText(QCoreApplication::translate("MainWindow", "\345\217\221\351\200\201", nullptr));
+        ExitButton->setText(QCoreApplication::translate("MainWindow", "\351\200\200\345\207\272", nullptr));
     } // retranslateUi
 
 };
